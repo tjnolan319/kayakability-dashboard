5c05830003dd6814c0e42c126751c6efbbba8c27
Tim Nolan <tjnolan319@gmail.com>
2025-06-28 12:09:48 -0400

    Update kayak_data_export.py

--- kayak_data_export.py
+++ kayak_data_export.py
@@ -14,13 +14,38 @@ merrimack_sites = {
         "ideal_gage_range": (2.0, 4.5)
     },
     "01100000": {
-        "name": "Merrimack River at Lowell, MA",
+        "name": "Merrimack River at Lowell, MA", 
         "lat": 42.65,
         "lon": -71.30,
         "ideal_discharge_range": (1000, 2500),
         "ideal_gage_range": (1.5, 5.0)
     },
-    # Add other sites as needed
+    "01096500": {
+        "name": "Merrimack River at North Chelmsford, MA",
+        "lat": 42.6278,
+        "lon": -71.3667,
+        "ideal_discharge_range": (800, 2200),
+        "ideal_gage_range": (2.0, 4.8)
+    },
+    "01094000": {
+        "name": "Merrimack River near Goffs Falls below Manchester, NH",
+        "lat": 43.0167,
+        "lon": -71.4833,
+        "ideal_discharge_range": (600, 1800),
+        "ideal_gage_range": (1.8, 4.2)
+    },
+    "01092000": {
+        "name": "Merrimack River at Franklin Junction, NH",
+        "lat": 43.4361,
+        "lon": -71.6472,
+        "ideal_discharge_range": (400, 1500),
+        "ideal_gage_range": (1.5, 3.8)
+    }
+}
+
+parameter_codes = {
+    '00060': 'discharge_cfs',
+    '00065': 'gage_height_ft'
 }
 
 parameter_codes = {'00060': 'discharge_cfs', '00065': 'gage_height_ft'}
