1b54b20dd2036d7bc368d4f10574c8aca59e14d2
Tim Nolan <tjnolan319@gmail.com>
2025-06-30 16:49:20 -0400

    Create site_config.py

---
+++ site_config.py
@@ -0,0 +1,42 @@
+merrimack_sites = {
+    "01073500": {
+        "name": "Merrimack River below Concord River at Lowell, MA",
+        "lat": 42.6334,
+        "lon": -71.3162,
+        "ideal_discharge_range": (800, 2000),
+        "ideal_gage_range": (2.0, 4.5),
+        "difficulty": "Class I-II"
+    },
+    "01100000": {
+        "name": "Merrimack River at Lowell, MA", 
+        "lat": 42.65,
+        "lon": -71.30,
+        "ideal_discharge_range": (1000, 2500),
+        "ideal_gage_range": (1.5, 5.0),
+        "difficulty": "Class I-II"
+    },
+    "01096500": {
+        "name": "Merrimack River at North Chelmsford, MA",
+        "lat": 42.6278,
+        "lon": -71.3667,
+        "ideal_discharge_range": (800, 2200),
+        "ideal_gage_range": (2.0, 4.8),
+        "difficulty": "Class I-II"
+    },
+    "01094000": {
+        "name": "Merrimack River near Goffs Falls below Manchester, NH",
+        "lat": 43.0167,
+        "lon": -71.4833,
+        "ideal_discharge_range": (600, 1800),
+        "ideal_gage_range": (1.8, 4.2),
+        "difficulty": "Class II"
+    },
+    "01092000": {
+        "name": "Merrimack River at Franklin Junction, NH",
+        "lat": 43.4361,
+        "lon": -71.6472,
+        "ideal_discharge_range": (400, 1500),
+        "ideal_gage_range": (1.5, 3.8),
+        "difficulty": "Class I"
+    }
+}
